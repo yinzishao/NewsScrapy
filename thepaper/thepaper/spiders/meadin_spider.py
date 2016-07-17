f81c5232075ad57877ded06a723516b43ea369dc
yinzishao <370953598@qq.com>
2016-07-17 16:09:08 +0800

    finish meadin parse news content

--- thepaper/thepaper/spiders/meadin_spider.py
+++ thepaper/thepaper/spiders/meadin_spider.py
@@ -10,8 +10,9 @@ import time
 import re
 import logging
 from thepaper.items import NewsItem
+from thepaper.settings import *
 logger = logging.getLogger("MeadinSpider")
-
+import pdb
 class MeadinSpider(scrapy.spiders.Spider):
     domain = "http://www.meadin.com/"
     name = "meadin"
@@ -19,10 +20,11 @@ class MeadinSpider(scrapy.spiders.Spider):
     start_urls = [
         "http://info.meadin.com/Index_1.shtml",
     ]
+    end_day = END_DAY
+    end_now = END_NOW
 
     def parse(self, response):
-        html = response.body
-        soup = BeautifulSoup(html,"lxml")
+        soup = BeautifulSoup(response.body,"lxml")
 
         #爬取列表
         viewlist = soup.find_all("div","list list-640")
@@ -30,7 +32,7 @@ class MeadinSpider(scrapy.spiders.Spider):
             for news in viewlist:
                 title = news.select("h3 a")[0].string if news.select("h3 a") else None
                 news_url = news.select("h3 a")[0].get("href",None) if news.select("h3 a") else None
-                content = news.select('p[class="info"]')[0].string if news.select('p[class="info"]') else None  #info
+                abstract = news.select('p[class="info"]')[0].string if news.select('p[class="info"]') else None  #info
                 pic = news.find('img').get("src",None) if news.find('img') else None    #图片链接
                 #brand
                 tags = []                           #标签组
@@ -39,16 +41,16 @@ class MeadinSpider(scrapy.spiders.Spider):
                     topic = fl.select("a")[0].string    #专题
                     for i in fl.select("a")[1:-1]:
                         tags.append(i.string)
-                    date = fl.find(class_="fr arial").string
+                    news_date = fl.find(class_="fr arial").string   #%Y-%m-%d
                 else:
-                    date = None
+                    news_date = None
                     topic=None
-                #新闻不是当天的
-                if time.strptime(date,"%Y-%m-%d").tm_mday != time.localtime().tm_mday:
-                    raise CloseSpider('today scrapy end')
 
-                news_item = NewsItem(title=title,news_url=news_url,content=content,pic=pic,topic=topic,time=date,tags=tags)
-                yield news_item
+
+                item = NewsItem(title=title,news_url=news_url,abstract=abstract,pic=pic,topic=topic,news_date=news_date)
+                request = scrapy.Request(news_url,callback=self.parse_news)
+                request.meta['item']=item
+                yield request
 
         else:
             logger.info("can't find news list")
@@ -61,3 +63,35 @@ class MeadinSpider(scrapy.spiders.Spider):
             yield scrapy.Request(new_url)
         else:
             logger.info("can't find index")
+
+    def parse_news(self,response):
+        #content,news_date,news_no,crawl_date,referer_web
+        item = response.meta.get("item",NewsItem())
+        soup = BeautifulSoup(response.body)
+        # news_date = item.get("news_date",None)
+        #需要爬取具体的时间
+        news_date = soup.find("span",class_="arial").text if soup.find("span",class_="arial") else None
+        #http://info.meadin.com/PictureNews/2938_1.shtml Exception
+        if news_date:
+
+            struct_date = datetime.datetime.strptime(news_date,"%Y-%m-%d %H:%M:%S")
+            delta = self.end_now-struct_date
+            if delta.days == self.end_day:
+                raise CloseSpider('today scrapy end')
+            referer_web = list(soup.find("p",class_="source").strings)[-1] if soup.find("p",class_="source") else None
+            #爬取正文
+            art,content = None,None
+            art = soup.find("div",class_="article js-article")
+            if art:
+                #剔除摘要！
+                art.find("div",class_="intro").replace_with("")
+                content =art.text.strip()
+            news_no =response.url.split("/")[-1].split("_")[0]
+            item["news_date"]=news_date
+            item["content"]=content
+            item["referer_web"]=referer_web
+            item["crawl_date"]=NOW
+            item["news_no"]=news_no
+            yield item
+        else:
+            logger.warning("can't find news_date.the url is %s" % response.url)
