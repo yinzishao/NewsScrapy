5b470851127bfc31a212e62247a88de726412c55
yinzishao <370953598@qq.com>
2016-07-25 23:44:31 +0800

    code refactoring：     extract the end codition accroding to the news date

--- thepaper/thepaper/util.py
+++ thepaper/thepaper/util.py
@@ -3,6 +3,8 @@
 __author__ = 'yinzishao'
 
 import time
+import datetime
+from thepaper.settings import *
 """
     判断传入的时间是否是当天
     request：
@@ -10,3 +12,19 @@ import time
 """
 def judge_today(date):
     return date.tm_mday == time.localtime().tm_mday
+
+def judge_news_crawl(item):
+    """
+    判断是否符合爬取的时间
+    :param item:
+    :return: item or None
+    """
+    news_date = item.get("news_date",None)
+    if news_date:
+        struct_date = datetime.datetime.strptime(news_date,"%Y-%m-%d %H:%M:%S")
+        delta = END_NOW-struct_date
+        if delta.days < END_DAY:
+            return item
+        else:
+            return None
+    return None
