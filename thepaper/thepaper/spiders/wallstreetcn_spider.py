8d0c740351c581417b26472d9d37748f4afe9d3c
yinzishao <370953598@qq.com>
2016-07-26 22:20:00 +0800

    wallstreetcn     use util judge the item according to news date

--- thepaper/thepaper/spiders/wallstreetcn_spider.py
+++ thepaper/thepaper/spiders/wallstreetcn_spider.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+from thepaper.util import judge_news_crawl
+
 __author__ = 'yinzishao'
 
 import re
@@ -20,12 +22,15 @@ class Wallstreetcn(scrapy.spiders.Spider):
     end_now = END_NOW
     crawl_page = 5  #爬取的页面限制
     index_page = 1  #开始页面数
+    flag = 0        #是否爬取下一页，当结束时flag为停止页面数
     page_url = "http://wallstreetcn.com/news?status=published&type=news&order=-created_at&limit=30&page=%s"
     start_urls = [
         page_url % index_page
     ]
     def parse(self,response):
         soup = BeautifulSoup(response.body,"lxml")
+        page_res = re.search("page=(\d+)",response.url)
+        pageindex = page_res.group(1) if page_res else None #爬取页数
         search_result = soup.find_all("ul",id="search-result")
         if search_result:
             news_list = search_result[0].find_all("li",class_="news")
@@ -36,36 +41,33 @@ class Wallstreetcn(scrapy.spiders.Spider):
                 abstract =None
                 if news.find("div",class_="summary hidden-xxs"):
                     abstract = news.find("div",class_="summary hidden-xxs").string.strip()
-                else:None
-
                 pic =news.find("img").get("data-original",None) if news.find("img") else None
                 item = NewsItem(news_url=news_url,abstract=abstract,pic =pic)
                 #将item作为元素传递到解析页面中
-                request = scrapy.Request(news_url,callback=self.parse_news)
-                request.meta['item'] = item
                 if news_url:
+                    request = scrapy.Request(news_url,callback=self.parse_news)
+                    request.meta['item'] = item
+                    request.meta['pageindex'] = pageindex
                     yield request
                 else:
                     logger.info("can't find news_url")
             #下一页
-            page_res = re.search("page=(\d+)",response.url)
-            page = page_res.group(1) if page_res else None
-            if int(page)<self.crawl_page:
-                next_url = self.page_url % str(int(page)+1)
+
+            # if int(pageindex)<self.crawl_page:
+            if not self.flag:
+                next_url = self.page_url % str(int(pageindex)+1)
                 yield scrapy.Request(next_url,callback=self.parse)
         else:
             logger.info("can't find search_result")
 
     def parse_news(self,response):
         item = response.meta['item']
+        pageindex = response.meta['pageindex']
         soup = BeautifulSoup(response.body,"lxml")
         #eg:2016年07月13日 21:50:42
         news_date = soup.find("span",class_="item time").string if soup.find("span",class_="item time") else None
         struct_date = datetime.datetime.strptime(news_date.encode('utf-8'),"%Y年%m月%d日 %H:%M:%S")
-        #超过结束限期，结束
-        delta = self.end_now-struct_date
-        if delta.days == self.end_day:
-            raise CloseSpider('today scrapy end')
+        news_date = struct_date.strftime("%Y-%m-%d %H:%M:%S")   #规范日期时间
         title = soup.find("h1",class_="article-title").string if soup.find("h1",class_="article-title") else None
         author = soup.find("span",class_="item author").a.string if soup.find("span",class_="item author") else None
         comment_num = soup.find("span",class_="wscn-cm-counter").string if soup.find("span",class_="wscn-cm-counter") else None
@@ -78,6 +80,10 @@ class Wallstreetcn(scrapy.spiders.Spider):
         item["news_date"]=news_date
         item["crawl_date"]=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         item["news_no"] = news_no
-        yield item
+        item = judge_news_crawl(item)   #判断是否符合爬取时间
+        if item:
+            yield item
+        else:
+            self.flag= pageindex
 
 
