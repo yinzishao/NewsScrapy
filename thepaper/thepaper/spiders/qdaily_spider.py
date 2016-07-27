d652b20942af87a9f2e50204da0adc49d80ceae5
yinzishao <370953598@qq.com>
2016-07-27 22:22:17 +0800

    qdaily catalogue and use util

--- thepaper/thepaper/spiders/qdaily_spider.py
+++ thepaper/thepaper/spiders/qdaily_spider.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+from thepaper.util import judge_news_crawl
 
 __author__ = 'yinzishao'
 
@@ -18,6 +19,8 @@ class QdailySpider(scrapy.spiders.Spider):
     allowed_domains = ["qdaily.com",]
     end_day = END_DAY     #终结天数
     end_now = END_NOW
+    top_flag = 0
+    com_flag = 0
     start_urls = [
         "http://www.qdaily.com/tags/29.html",    #top15
         "http://www.qdaily.com/categories/18.html", #商业
@@ -26,44 +29,59 @@ class QdailySpider(scrapy.spiders.Spider):
     def parse(self, response):
         soup = BeautifulSoup(response.body,"lxml")
         newslist = soup.find(name="div", attrs={"data-lastkey": True})
-        if newslist:
-
-            for i in newslist.children:
-                #文章中间有其余无关信息
-                if i != u' ':
-                    news_url = self.domain+i.a.get('href',None)
-                    pic = i.find("img").get('data-src') if i.find("img") else None
-                    title = i.find("h3").string if i.find("h3") else None
-                    comment_num = i.find(class_="iconfont icon-message").string if i.find(class_="iconfont icon-message") else 0
-                    heart = i.find(class_="iconfont icon-heart").string if i.find(class_="iconfont icon-heart") else 0
-                    topic = i.find(class_="category").span.string if i.find(class_="category") else 0
-                    news_date =None
-                    if i.find(name="span", attrs={"data-origindate": True}):
-                        news_date= i.find(name="span", attrs={"data-origindate": True}).get("data-origindate",None)
-                        if news_date:
-                            news_date = news_date[:-6]
-
-                    #no content and have heart&conment but not add
-                    item = NewsItem(title=title,news_url=news_url,pic=pic,topic=topic,news_date=news_date,comment_num=comment_num)
-                    request = scrapy.Request(news_url,callback=self.parse_article)
-                    request.meta["item"] = item
-                    yield request
-            lastkey = newslist.get("data-lastkey",None)
-            logger.info(lastkey)
-            if lastkey:
-                if "tags" in response.url:
-                    next_url = "http://www.qdaily.com/tags/tagmore/29/%s.json" % lastkey
-                else:
-                    next_url = "http://www.qdaily.com/categories/categorymore/18/%s.json" % lastkey
-                # logger.info(next_url)
-                yield scrapy.Request(next_url,callback=self.parse_next_page)
-            else:
-                logger.warning("can't find next page")
+        lastkey = newslist.get("data-lastkey",None)
+        logger.info(lastkey)
+        if not lastkey:
+            logger.warning("can't find next page")
         else:
-            logger.warning("can't find newslit")
+            if newslist:
+                for i in newslist.children:
+                    #文章中间有其余无关信息
+                    if i != u' ':
+                        news_url = self.domain+i.a.get('href',None)
+                        pic = i.find("img").get('data-src') if i.find("img") else None
+                        title = i.find("h3").string if i.find("h3") else None
+                        comment_num = i.find(class_="iconfont icon-message").string if i.find(class_="iconfont icon-message") else 0
+                        heart = i.find(class_="iconfont icon-heart").string if i.find(class_="iconfont icon-heart") else 0
+                        topic = i.find(class_="category").span.string if i.find(class_="category") else 0
+                        news_date =None
+                        if i.find(name="span", attrs={"data-origindate": True}):
+                            news_date= i.find(name="span", attrs={"data-origindate": True}).get("data-origindate",None)
+                            if news_date:
+                                news_date = news_date[:-6]
+
+                        #no content and have heart&conment but not add
+                        item = NewsItem(title=title,news_url=news_url,pic=pic,topic=topic,news_date=news_date,comment_num=comment_num)
+                        # 所属目录
+                        item['catalogue'] = "Top 15" if "tags" in response.url else u"商业"
+                        #判断是否结束
+                        item = judge_news_crawl(item)
+                        if item :
+                            request = scrapy.Request(news_url,callback=self.parse_article)
+                            request.meta["item"] = item
+                            yield request
+                        else:
+                            if "tags" in response.url:
+                                self.top_flag = lastkey
+                            else:
+                                self.com_flag = lastkey
+                next_url = None
+                #判断各个类别是否需要爬取下一页
+                if "tags" in response.url:
+                    if not self.top_flag:
+                        next_url = "http://www.qdaily.com/tags/tagmore/29/%s.json" % lastkey
+                else:
+                    if not self.com_flag:
+                        next_url = "http://www.qdaily.com/categories/categorymore/18/%s.json" % lastkey
+                # logger.info(next_url)
+                if next_url:
+                    yield scrapy.Request(next_url,callback=self.parse_next_page)
+            else:
+                logger.warning("can't find newslit")
     def parse_next_page(self,response):
         data = json.loads(response.body)
         newslist = data['data']["feeds"]
+        last_key = data['data']['last_key'] if data['data']['has_more'] else None
         for news in newslist:
             post = news.get("post",None)
             if post:
@@ -71,7 +89,7 @@ class QdailySpider(scrapy.spiders.Spider):
 
                 pic = post.get("image",None)
                 title = post.get("title",None)
-                comment_count = post.get("comment_count",None)
+                comment_num = post.get("comment_count",None)
                 praise_count = post.get("praise_count",None)    #heart
                 topic = post['category']['title']
                 id = post.get("id",None)
@@ -83,17 +101,27 @@ class QdailySpider(scrapy.spiders.Spider):
                 #文章
                 if id and datatype:
                     news_url = self.domain+"%s/%s" % (datatype+"s",id)
-                    item = NewsItem(title=title,news_url=news_url,pic=pic,topic=topic,news_date=news_date)
-                    request = scrapy.Request(news_url,callback=self.parse_article)
-                    request.meta['item'] = item
-                    yield request
+                    item = NewsItem(title=title,news_url=news_url,pic=pic,topic=topic,news_date=news_date,comment_num=comment_num)
+                    item['catalogue'] = "Top 15" if "tags" in response.url else u"商业"
+                    item = judge_news_crawl(item)
+                    if item :
+                        request = scrapy.Request(news_url,callback=self.parse_article)
+                        request.meta["item"] = item
+                        yield request
+                    else:
+                        if "tags" in response.url:
+                            self.top_flag = last_key
+                        else:
+                            self.com_flag = last_key
         #下一页
-        if data['data']['has_more']:
-            last_key = data['data']['last_key']
-            if "tags" in response.url:
+        next_url = None
+        if "tags" in response.url:
+            if not self.top_flag:
                 next_url = "http://www.qdaily.com/tags/tagmore/29/%s.json" % last_key
-            else:
+        else:
+            if not self.com_flag:
                 next_url = "http://www.qdaily.com/categories/categorymore/18/%s.json" % last_key
+        if next_url:
             yield scrapy.Request(next_url,callback=self.parse_next_page)
 
 
@@ -101,13 +129,13 @@ class QdailySpider(scrapy.spiders.Spider):
     def parse_article(self,response):
         #content,news_no,crawl_date
         item = response.meta.get("item",NewsItem())
-        news_date = item.get("news_date",None)
-        if news_date:
-            struct_date = datetime.datetime.strptime(news_date,"%Y-%m-%d %H:%M:%S")
-            delta = self.end_now-struct_date
-            print delta.days
-            if delta.days == self.end_day:
-                raise CloseSpider('today scrapy end')
+        # news_date = item.get("news_date",None)
+        # if news_date:
+        #     struct_date = datetime.datetime.strptime(news_date,"%Y-%m-%d %H:%M:%S")
+        #     delta = self.end_now-struct_date
+        #     print delta.days
+        #     if delta.days == self.end_day:
+        #         raise CloseSpider('today scrapy end')
         soup =BeautifulSoup(response.body)
         author = soup.find("span",class_="name").text if soup.find("span",class_="name") else None
         abstract =  soup.find("p",class_="excerpt").text if soup.find("p",class_="excerpt") else None
