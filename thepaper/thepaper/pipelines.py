cc23ccae29e92746a4dbe6a740a8660e86f17833
yizishao <yinzishao@youmi.net>
2016-10-31 22:35:35 +0800

    添加一些配置和pipeline的简单处理

--- thepaper/thepaper/pipelines.py
+++ thepaper/thepaper/pipelines.py
@@ -6,6 +6,42 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class ThepaperPipeline(object):
+import json
+import pymongo
+
+class JsonWriterPipeline(object):
+
+    def __init__(self):
+        self.file = open('items.jl', 'wb')
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
+        return item
+
+
+class MongoPipeline(object):
+
+    collection_name = 'scrapy_items'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        self.db[self.collection_name].insert(dict(item))
         return item
--- thepaper/thepaper/settings.py
+++ thepaper/thepaper/settings.py
@@ -65,9 +65,9 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'thepaper.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'thepaper.pipelines.JsonWriterPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
